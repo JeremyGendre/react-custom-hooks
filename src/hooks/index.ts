b0b584d8b8bf04a749c63ef04e87c5a2b3916bcb
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-06-18 14:54:22 +0200

    feat: useFetch

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useClickAway } from "./useClickAway";
 export { default as useLog } from './useLog';
 export { default as useTimeout } from './useTimeout';
 export { default as useInterval } from './useInterval';
+export { default as useFetch } from './useFetch';
